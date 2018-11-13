47e669d2c83a295770e28f91578967c9df6d4001
Nathan Westfall <nathan@fistbumpstudios.com>
2018-11-13 11:10:23 -0500

    wrote unit tests for app logic

---
+++ AzureDevOps.UnitTests/Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using AzureDevOps.Models;
+
+namespace AzureDevOps.UnitTests
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestLogicModelCase()
+		{
+			var logicModel = new LogicModel();
+			Assert.AreEqual(0, logicModel.X);
+			Assert.AreEqual(0, logicModel.Y);
+			Console.WriteLine($"Logic Model #1: {logicModel.X}, {logicModel.Y}");
+
+			logicModel = new LogicModel(1, 2);
+			Assert.AreEqual(1, logicModel.X);
+			Assert.AreEqual(2, logicModel.Y);
+			Console.WriteLine($"Logic Model #2: {logicModel.X}, {logicModel.Y}");
+		}
+	}
+}
---
+++ AzureDevOps/Models/LogicModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace AzureDevOps.Models
+{
+	public class LogicModel
+	{
+		public double X { get; set; }
+
+		public double Y { get; set; }
+
+		public LogicModel()
+		{
+			X = 0;
+			Y = 0;
+		}
+
+		public LogicModel(double x, double y)
+		{
+			X = x;
+			Y = y;
+		}
+	}
+}
